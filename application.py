2c050acb7ebeeab38e1c097c587017893c9cf8ac
Jeffrey Xu <jeffrey@xuyaowu.com>
2021-04-21 17:12:42 -0400

    basic databases

---
+++ application.py
@@ -0,0 +1,31 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+        # print(request.form.get("month"))
+        id = db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", request.form.get("name"), request.form.get("month"), request.form.get("day"))
+
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+
+        return render_template("index.html", entries = db.execute("SELECT * FROM birthdays"))
+
+
